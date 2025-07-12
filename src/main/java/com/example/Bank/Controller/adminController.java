6cc9f5fc14bc6e37f18365d0e5f32e859e88babd
KamaleshMani <kamalesh282856@gmail.com>
2025-07-12 19:47:21 +0530

    initial commit

--- src/main/java/com/example/Bank/Controller/adminController.java
+++ src/main/java/com/example/Bank/Controller/adminController.java
@@ -1,4 +1,24 @@
 package com.example.Bank.Controller;
 
+import com.example.Bank.Entities.Admin;
+import com.example.Bank.Entities.Client;
+import com.example.Bank.Entities.Transaction;
+import com.example.Bank.Service.adminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//
+//public class adminController {
+//}
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
 public class adminController {
+
 }
+
--- src/main/java/com/example/Bank/Service/adminService.java
+++ src/main/java/com/example/Bank/Service/adminService.java
@@ -1,4 +1,27 @@
 package com.example.Bank.Service;
 
+import com.example.Bank.Entities.Account;
+import com.example.Bank.Entities.Admin;
+import com.example.Bank.Entities.Client;
+import com.example.Bank.Entities.Transaction;
+import com.example.Bank.Repository.accountRepository;
+import com.example.Bank.Repository.adminRepository;
+import com.example.Bank.Repository.clientRepository;
+import com.example.Bank.Repository.transactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class adminService {
+    private  clientRepository c_repo;
+    private accountRepository ac_repo;
+    private  transactionRepository tr_repo;
+    private adminRepository a_repo;
+
+
 }
